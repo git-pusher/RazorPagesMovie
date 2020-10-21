1bb4b75800c3ef65e50ec0d84258efc799ef5d16
git-pusher <rachel.palomeque@gmail.com>
2020-10-21 12:26:23 -0500

    Agregar archivos de proyecto.

---
+++ Models/Movie.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RazorPagesMovie.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
+        [Display(Name = "Título")]
+        public string Title { get; set; }
+
+
+        [Display(Name = "Fecha de lanzamiento")]
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+
+        //[RegularExpression(@"^[A-Z]+[a-zA-Z]*$")]
+        [Required]
+        [StringLength(30)]
+        [Display(Name = "Género")]
+        public string Genre { get; set; }
+
+        [Range(1, 100)]
+        [DataType(DataType.Currency)]
+        [Display(Name = "Precio")]
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Price { get; set; }
+
+        //[RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+        [StringLength(5)]
+        [Display(Name = "Calificación")]
+        public string Rating { get; set; }
+    }
+}
